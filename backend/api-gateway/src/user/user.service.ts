8e5492b815a5e0f1af596ed112ddd477b3802f6b
Rafael Vasconcellos de Medeiros <rvmedeirosengineer@gmail.com>
2025-05-08 14:25:45 -0300

    chore: default files to running

--- backend/api-gateway/src/user/user.service.ts
+++ backend/api-gateway/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import * as bcrypt from "bcrypt";
@@ -6,13 +6,43 @@ import * as bcrypt from "bcrypt";
 import { User } from "./user.entity";
 import { CreateUserDto } from "./create-user.dto";
 
-
 @Injectable()
-export class UserService {
+export class UserService implements OnModuleInit {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>
   ) {}
+
+  private async ensureAdminUser() {
+    const adminUsername = "admin";
+    const adminPassword = "admin123";
+    const adminRole = "admin";
+
+    const existingAdmin = await this.userRepository.findOne({
+      where: { username: adminUsername },
+    });
+
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash(adminPassword, 10);
+      const adminUser = this.userRepository.create({
+        username: adminUsername,
+        password: hashedPassword,
+        role: adminRole,
+        isActive: true,
+      });
+
+      await this.userRepository.save(adminUser);
+      this.logger.log("Admin user created successfully");
+    } else {
+      this.logger.log("Admin user already exists");
+    }
+  }
+
+  async onModuleInit() {
+    await this.ensureAdminUser();
+  }
 
   async createUser(userDto: CreateUserDto): Promise<User> {
     const hashedPassword = await bcrypt.hash(userDto.password, 10);
